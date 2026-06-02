1d94d82b7b1cdb9ee74a29c8a34d013e92c41b73
jeevan8091 <jeevanyabesh@gmail.com>
2026-06-02 16:09:18 +0530

    feat(day 2): create login page and configure routes

---
+++ src - Copy/App.jsx
@@ -0,0 +1,11 @@
+import Table from './components/Table';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-slate-50 via-blue-50 to-indigo-50 py-12 px-4 sm:px-6 lg:px-8 font-sans antialiased text-gray-800">
+      <Table />
+    </div>
+  );
+}
+
+export default App;
